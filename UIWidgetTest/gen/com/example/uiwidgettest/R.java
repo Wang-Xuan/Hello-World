8346b776dfad11cc64fd78b07fe4995097a9d392
Wang-Xuan <wxlayun36@gmail.com>
2014-12-07 15:51:30 +0800

    第三章

--- UIWidgetTest/gen/com/example/uiwidgettest/R.java
+++ UIWidgetTest/gen/com/example/uiwidgettest/R.java
@@ -23,9 +23,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int jelly_ben=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int button=0x7f080001;
+        public static final int edit_text=0x7f080002;
+        public static final int image_view=0x7f080003;
+        public static final int progress_bar=0x7f080004;
+        public static final int text_view=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- UIWidgetTest/src/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/src/com/example/uiwidgettest/MainActivity.java
@@ -1,16 +1,104 @@
 package com.example.uiwidgettest;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private Button button;
+	private EditText editText;
+	private ImageView imageView;
+	private ProgressBar progressBar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		button = (Button) findViewById(R.id.button);
+		editText = (EditText) findViewById(R.id.edit_text);
+		imageView = (ImageView) findViewById(R.id.image_view);
+		progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+		button.setOnClickListener(this);
+
+		// 1.匿名类(P89)
+		/*	button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// 在此添加逻辑
+			}
+		});*/
+	}
+	
+	// 2.通过接口实现(P89)
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button:
+			// 在此添加逻辑
+			
+			// 输入框提示性文字(EditText|P91)
+			/*String inputText = editText.getText().toString();
+			Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();*/
+			
+			// 通过代码动态地更改图片(ImageView|P96)
+			/*mageView.setImageResource(R.drawable.jelly_ben);*/
+			
+			// 通过代码实现进度条的可见属性(ProgressBar|(P98))
+			/*if (progressBar.getVisibility() == View.GONE) {
+				progressBar.setVisibility(View.VISIBLE);
+			}else{
+				progressBar.setVisibility(View.GONE);
+			}*/
+			
+			// 水平进度条显示(ProgressBar|(P100))
+			/*int progress = progressBar.getProgress();
+			progress = progress + 10;
+			progressBar.setProgress(progress);*/
+			
+			// AlerDialog(P101)
+			/*AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+			dialog.setTitle("This is Dialog");
+			dialog.setMessage("Something important.");
+			dialog.setCancelable(false);
+			dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+				}
+			});
+			dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {					
+				}
+			});
+			dialog.show();*/
+			
+			// ProgressDialog(P103)
+			ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+			progressDialog.setTitle("This is ProgressDialog");
+			progressDialog.setMessage("Loading...");
+			progressDialog.setCancelable(true);
+			progressDialog.show();
+			
+			break;
+		default:
+			break;
+		}
 	}
 
 	@Override
