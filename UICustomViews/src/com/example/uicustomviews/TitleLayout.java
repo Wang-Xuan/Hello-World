50206790a538560bca3651bf4763b37bc30e9159
Wang-Xuan <wxlayun36@gmail.com>
2014-12-08 17:14:10 +0800

    第三章

---
+++ UICustomViews/src/com/example/uicustomviews/TitleLayout.java
@@ -0,0 +1,16 @@
+package com.example.uicustomviews;
+
+import android.content.Context;
+import android.text.style.SuperscriptSpan;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+public class TitleLayout extends LinearLayout {
+
+	public TitleLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		LayoutInflater.from(context).inflate(R.layout.title, this);
+	}
+	
+}
