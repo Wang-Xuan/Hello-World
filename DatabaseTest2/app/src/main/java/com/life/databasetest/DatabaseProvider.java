14f9b0026f81d5609eb609d458bac0ef3e2d3f89
Wang-Xuan <wxlayun36@gmail.com>
2015-03-13 21:02:13 +0800

    第七章

--- DatabaseTest2/app/src/main/java/com/life/databasetest/DatabaseProvider.java
+++ DatabaseTest2/app/src/main/java/com/life/databasetest/DatabaseProvider.java
@@ -1,11 +1,11 @@
 package com.life.databasetest;
 
-import android.content.ContentProvider;
-import android.content.ContentValues;
-import android.content.UriMatcher;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
+        import android.content.ContentProvider;
+        import android.content.ContentValues;
+        import android.content.UriMatcher;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.net.Uri;
 
 /**
  * Created by lixfe on 2015/3/12.
@@ -24,7 +24,7 @@ public class DatabaseProvider extends ContentProvider {
 
     private static UriMatcher uriMatcher;
 
-    private MyDatabaseHelper dpHelper;
+    private MyDatabaseHelper dbHelper;
 
     static {
         uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
@@ -36,14 +36,14 @@ public class DatabaseProvider extends ContentProvider {
 
     @Override
     public boolean onCreate() {
-        dpHelper = new MyDatabaseHelper(getContext(), "BookStore.db", null, 2);
+        dbHelper = new MyDatabaseHelper(getContext(), "BookStore.db", null, 2);
         return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         // 查询数据
-        SQLiteDatabase db = dpHelper.getReadableDatabase();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor cursor = null;
         switch (uriMatcher.match(uri)) {
             case BOOK_DIR:
@@ -68,21 +68,91 @@ public class DatabaseProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        // 添加数据
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Uri uriReturn = null;
+        switch (uriMatcher.match(uri)){
+            case BOOK_DIR:
+            case BOOK_ITEM:
+                long newBookId = db.insert("Book", null, values);
+                uriReturn = Uri.parse("content://" + AUTHORITY + "/book/" + newBookId);
+                break;
+            case CATEGORY_DIR:
+            case CATEGORY_ITEM:
+                long newCategoryId = db.insert("Category", null, values);
+                uriReturn = Uri.parse("content://" + AUTHORITY + "/category" + newCategoryId);
+                break;
+            default:
+                break;
+        }
+        return uriReturn;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        // 更新数据
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int updatedRowd = 0;
+        switch (uriMatcher.match(uri)){
+            case BOOK_DIR:
+                updatedRowd = db.update("Book", values, selection, selectionArgs);
+                break;
+            case BOOK_ITEM:
+                String bookId = uri.getPathSegments().get(1);
+                updatedRowd = db.update("Book", values, "id = ?", new String[] { bookId });
+                break;
+            case CATEGORY_DIR:
+                updatedRowd = db.update("Category", values, selection, selectionArgs);
+                break;
+            case CATEGORY_ITEM:
+                String categoryId = uri.getPathSegments().get(1);
+                updatedRowd = db.update("Category", values, "id = ?", new String[] { categoryId });
+                break;
+            default:
+                break;
+        }
+        return updatedRowd;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        // 删除数据
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int deletedRows = 0;
+        switch (uriMatcher.match(uri)){
+            case BOOK_DIR:
+                deletedRows = db.delete("Book", selection, selectionArgs);
+                break;
+            case BOOK_ITEM:
+                String bookId = uri.getPathSegments().get(1);
+                deletedRows = db.delete("Book", "id = ?", new String[] { bookId });
+                break;
+            case CATEGORY_DIR:
+                deletedRows = db.delete("Category", selection, selectionArgs);
+                break;
+            case CATEGORY_ITEM:
+                String categoryId = uri.getPathSegments().get(1);
+                deletedRows = db.delete("Category", "id = ?", new String[] { categoryId });
+                break;
+            default:
+                break;
+        }
+        return deletedRows;
     }
 
     @Override
     public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)){
+            case BOOK_DIR:
+                return "vnd.android.cursor.dir/vnd.com.life.databasetest.provider.book";
+            case BOOK_ITEM:
+                return  "vnd.android.cursor.item/vnd.com.life.databasetest.provider.book";
+            case CATEGORY_DIR:
+                return "vnd.android.cursor.dir/vnd.com.life.databasetest.provider.category";
+            case CATEGORY_ITEM:
+                return "vnd.android.cursr.item/vnd.com.life.databasetest.provider.category";
+        }
+
         return null;
     }
 }
