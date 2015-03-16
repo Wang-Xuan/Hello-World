6d370c1b2fb6d269170313a276110e61d9f624a1
Wang-Xuan <wxlayun36@gmail.com>
2015-03-16 22:34:57 +0800

    第八章

--- AndroidThreadTest/app/src/main/java/com/life/androidthreadtest/MainActivity.java
+++ AndroidThreadTest/app/src/main/java/com/life/androidthreadtest/MainActivity.java
@@ -1,17 +1,45 @@
 package com.life.androidthreadtest;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.logging.Handler;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    public static final int UPDATE_TEXT = 1;
+
+    private TextView text;
+
+    private Button changeText;
+
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_TEXT:
+                    text.setText("Nice to meet you");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.text);
+        changeText = (Button) findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
     }
 
 
@@ -35,5 +63,24 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        // 将Message对象发送出去
+                        handler.sendMessage(message);
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
     }
 }
